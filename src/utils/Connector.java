8d075341ab6494547da7c7948fafb3ba77d741ab
Adis Veletanlic <adis.veletanlic@gmail.com>
2022-02-11 20:38:39 +0100

    Working on moving stuff to classes

--- src/utils/Connector.java
+++ src/utils/Connector.java
@@ -4,12 +4,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-public class Connector {
+
+public class Connector implements ConnectionUtils{
     private String hostname;
     private String database;
     private String conn_user;
     private String conn_pass;
     private Connection connection;
+    private Statement statement;
 
     public Connector(String hostname, String database, User user){
         this.hostname = hostname;
@@ -18,16 +20,61 @@ public class Connector {
         this.conn_user = user.getName();
         try{
             this.connection = init();
+            this.statement = connection.createStatement();
             
         }catch(Exception exception){
             exception.printStackTrace();
         }
-    
     }
     
     private Connection init() throws SQLException{
         return DriverManager.getConnection(String.format("jdbc:mysql://%s:3306/%s", hostname, database), 
             String.format("%s", conn_user), String.format("%s", conn_pass));
+    }
+
+    @Override
+    public ResultSet query(String sqlString) {
+        try {
+            return statement.executeQuery(sqlString);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public String displayQuery(ResultSet result) {
+        return null;
+    }
+
+    @Override
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        };
+        
+    }
+
+    @Override
+    public Boolean isConnected(){
+        try {
+            return connection.isValid(3);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public Connection getConnection() {
+        return this.connection;
+    }
+
+    @Override
+    public String getHost() {
+        return this.hostname;
     }
 
 }
