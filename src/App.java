caf19cacfd6dfd1890a1eeaede3bbf6d1cfe176e
Adis Veletanlic <adis.veletanlic@gmail.com>
2022-02-11 16:48:38 +0100

    First commit

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class App {
+
+    public static void main(String[] args) {
+        try{
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "guest", "q18jpg!5&rT");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM Country LIMIT 5");
+            while(rs.next()) System.out.println(rs.getString("Code"));
+        }catch (SQLException e) {
+            e.printStackTrace();
+        } {
+
+        }
+    }
+}
